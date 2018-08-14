77d1b26d1c6501899e46e511a7db2ec1e4e743de
Bogdan Preda <bogdan.preda@themeisle.com>
2018-08-14 14:41:19 +0300

    chore: vue cli project src only

---
+++ src/views/components/Navigation.vue
@@ -0,0 +1,79 @@
+<template>
+    <section class="">
+        <div class="container">
+            <!-- Navigation -->
+            <h2 class="mb-5">
+                <span>Navbars</span>
+            </h2>
+        </div>
+        <!-- Navbar default -->
+        <base-nav v-for="type in menuTypes"
+                   :key="type"
+                   :type="type"
+                   effect="dark"
+                   expand
+                   title="Default Color"
+                   class="mt-4"
+                   :content-id="`navbar-${type}`">
+            <div class="row" slot="content-header" slot-scope="{closeMenu}">
+                <div class="col-6 collapse-brand">
+                    <a href="./index.html">
+                        <img src="img/brand/blue.png">
+                    </a>
+                </div>
+                <div class="col-6 collapse-close">
+                    <close-button @click="closeMenu" :target="`navbar-${type}`">
+
+                    </close-button>
+                </div>
+            </div>
+
+            <ul class="navbar-nav ml-lg-auto">
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="#">
+                        <i class="ni ni-favourite-28"></i>
+                        <span class="nav-link-inner--text d-lg-none">Discover</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="#">
+                        <i class="ni ni-notification-70"></i>
+                        <span class="nav-link-inner--text d-lg-none">Profile</span>
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link nav-link-icon" href="#" id="navbar-default_dropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="ni ni-settings-gear-65"></i>
+                        <span class="nav-link-inner--text d-lg-none">Settings</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbar-default_dropdown_1">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+            </ul>
+
+        </base-nav>
+    </section>
+
+</template>
+<script>
+import BaseNav from "@/components/BaseNav";
+import CloseButton from "@/components/CloseButton";
+
+export default {
+  components: {
+    BaseNav,
+    CloseButton
+  },
+  data() {
+    return {
+      menuTypes: ["default", "primary", "success", "danger", "warning", "info"]
+    };
+  }
+};
+</script>
+<style>
+</style>
